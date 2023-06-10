e5f55a58f675f9d002fdebe59d0694c58293fa38
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2023-06-10 17:10:42 +0530

    contact

--- pages/mailme.php
+++ pages/mailme.php
@@ -2,9 +2,6 @@
 $email = $_POST["email"];
 $msg = $_POST["message"];
 
-$to = "chat.chillfeast@gmail.com";
-$sub = "New Enquiry";
-$body = "email:".$email."\nemail:".$msg;
-
-mail($to, $sub, $body);
+echo $email;
+echo $msg;
 ?>
